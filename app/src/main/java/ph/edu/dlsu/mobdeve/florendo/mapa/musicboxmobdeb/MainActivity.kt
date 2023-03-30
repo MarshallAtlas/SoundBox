31d71474ee7d8cc2dac6c315d0e78a683f4e250b
MarshallAtlas <matthew_florendo@dlsu.edu.ph>
2023-03-30 17:22:26 +0800

    Soundbox 1

---
+++ app/src/main/java/ph/edu/dlsu/mobdeve/florendo/mapa/musicboxmobdeb/MainActivity.kt
@@ -0,0 +1,11 @@
+package ph.edu.dlsu.mobdeve.florendo.mapa.musicboxmobdeb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
